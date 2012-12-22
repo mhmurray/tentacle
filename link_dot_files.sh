7c73c9a75320378a158d53a25b3c59323e8d9724
Michael Murray <mhmurray@uw.edu>
2012-12-22 13:33:37 -0500

    Fixed problems with /bin/sh invocation

--- link_dot_files.sh
+++ link_dot_files.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-dotfiles=".bashrc .profile .bash_login .bash_profile"
+dotfiles=".bashrc .bash_login .bash_profile"
 prefix=$HOME
 
 this_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# .profile is special. Since /bin/sh sources this, and sh is not 
+# compatible with bash commands necessarily (e.g. source vs .), 
+# we need to remove .profile completely.
+if [ -e ${HOME}/.profile ]; then
+  echo "Warning! ~/.profile exists! Please remove or rename this file to avoid incompatability between bash and sh."
+  exit 1
+fi
+
 
 # Verify that we aren't overwriting files or links
 for file in $dotfiles; do
